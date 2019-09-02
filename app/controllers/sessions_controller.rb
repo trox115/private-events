abd37c7c6af992b7e084f569b99d2b66008f976d
trox115 <tofernandes67@gmail.com>
2019-09-02 23:55:05 +0100

    users can login through ID

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
+  protect_from_forgery with: :exception
+  include SessionsHelper
+
   def new
+  end
+  def create
+    @user = User.find_by(name: params[:session][:name])
+    if @user
+      log_in(@user)
+      flash[:success] = 'Log in successful.'
+      redirect_to @user
+
+    else
+      flash.now[:danger] = 'The user id you entered does not exist.'
+      render 'new'
+    end
+  end
+  def destroy
   end
 end
