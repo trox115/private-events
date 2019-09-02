e46659b1927342f534edaf7cc3a38d8d9c81e57d
trox115 <tofernandes67@gmail.com>
2019-09-02 23:57:34 +0100

    made current user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 module SessionsHelper
   def log_in(user)
     session[:name] = user.name
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 end
