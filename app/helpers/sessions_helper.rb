795904bbe3fef1998f994918e8ea73cfaadf7b25
trox115 <tofernandes67@gmail.com>
2019-09-03 00:17:17 +0100

    user getting name when logged in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,11 +1,15 @@
-# frozen_string_literal: true
-
 module SessionsHelper
   def log_in(user)
     session[:name] = user.name
   end
-
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    if (name = session[:name])
+      @current_user ||= User.find_by(name: name)
+    end
   end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
 end
