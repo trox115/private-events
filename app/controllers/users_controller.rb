d63ca4ab4694aaed0554dc1c5c5a7df223c32aa1
Carlos Del Real <carloshdelreal@gmail.com>
2019-09-03 16:55:47 -0500

    Setup rspec as testing for the project

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,29 +3,29 @@
 class UsersController < ApplicationController
   def index
     @users=User.all
-    end
-  
-    def show
-      @user = User.find(params[:id])
-    end
-  
-    def new
-      @user=User.new
-    end
-  
-    def create
-      @user=User.new(user_params)
-      if @user.save
-        flash[:success] = "Welcome to the Events App, #{@user.name}!"
-        redirect_to @user
-      else
-        render 'new'
-      end
-    end
-  
-    private
-  
-    def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user=User.new
+  end
+
+  def create
+    @user=User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Events App, #{@user.name}!"
+      redirect_to @user
+    else
+      render 'new'
     end
   end
+
+  private
+
+  def user_params
+  params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
