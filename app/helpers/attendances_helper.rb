27c8a5db70d20bf8a947425d83531c243ab49fe9
trox115 <tofernandes67@gmail.com>
2019-09-05 00:46:50 +0100

    changed little bug

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -2,4 +2,8 @@ module AttendancesHelper
   def retrieve_name_event(atendances)
     Event.find_by(id: @user_attendance.ids).title
    end
+
+   def retrieve_id_event(atendances)
+    Event.find_by(id: @user_attendance.ids).id
+   end
 end
