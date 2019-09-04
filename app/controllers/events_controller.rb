5c7dd0aa682f35f5a6e58a753f869e9aeca96728
Carlos Del Real <carloshdelreal@gmail.com>
2019-09-04 15:01:50 -0500

    Events, show, new and create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,27 @@
 class EventsController < ApplicationController
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+    if @event.save
+      flash[:success] = "New Event Created, #{@event.title}!"
+      redirect_to current_user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description)
+  end
+
 end
