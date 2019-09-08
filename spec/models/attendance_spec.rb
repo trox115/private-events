3f73ec774b91852ad67d02b3027942235b7d2e61
Carlos Del Real <carloshdelreal@gmail.com>
2019-09-08 17:25:15 -0500

    Start with the testing

--- spec/models/attendance_spec.rb
+++ spec/models/attendance_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Attendance, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Create a user with valid information' do
+    it 'Creates the user' do
+      user = User.new(
+        name: "carlos",
+        email: "carlos@gmail.com",
+        password: "12345678",
+        password_confirmation: "12345678"
+      )
+      expect(user).to be_valid
+    end
+
+    it 'Is invalid without a name' do
+      user = User.new(name: nil)
+      user.valid?
+      expect(user.errors[:name]).to include("can't be blank")
+    end
+  end
 end
