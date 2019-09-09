08ecc2ea8a0da27bec591b80253a8e94fd03105e
trox115 <tofernandes67@gmail.com>
2019-09-10 00:49:28 +0100

    added user in factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,9 @@
 FactoryBot.define do
-  factory :user do
-    name "carlos"
-    email "carlos@email.com"
-    password "12345678"
-  end
+  
 
-  factory :antonio do
-    name 'antonio'
-    email 'antonio@email.com'
+  factory :user do
+    sequence(:name) { |n| "user#{n}" }
+    sequence(:email) {|n| "user#{n}@mail.com"}
     password '12345678'
-  end
+    end
 end
--- spec/models/attendance_spec.rb
+++ spec/models/attendance_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe Attendance, type: :model do
   context 'User creates and event and add invited users' do
     it 'Creates the user, the event and ivites users' do
-      #user = FactoryBot.build(:antonio)
+      user = FactoryBot.create(:user)
+      user.valid?
     end
   end
 end
