092635a14dab9287a28b9ecb408c49ae8da856a5
trox115 <tofernandes67@gmail.com>
2019-09-09 22:45:58 +0100

    pending tests corrected

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,10 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  describe "#new" do
-    it "responds successfully" do
-      #get :new#, params: { user: User.new }
-      #expect(response).to be_success
-    end
-  end
+ 
 end
