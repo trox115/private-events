03c263f336836dc2fece5ccd02d17678231c7dc8
Carlos Del Real <carloshdelreal@gmail.com>
2019-09-10 14:26:51 -0500

    logic working but confusing, planning to improve readability

--- spec/models/attendance_spec.rb
+++ spec/models/attendance_spec.rb
@@ -5,6 +5,15 @@ RSpec.describe Attendance, type: :model do
     it 'Creates the user, the event and ivites users' do
       user = FactoryBot.create(:user)
       user.valid?
+
+      e = user.events.build(title: "After Club Party", description: "The most wonderful party")
+      expect(e.attendees).to be_empty
+      5.times do
+        e.attendees << FactoryBot.create(:user)
+      end
+      expect(e.attendees.length).to eq 5
+      expect(e.attendances.length).to eq 5
+      expect(e.attendances.first.attending).to eq false
     end
   end
 end
