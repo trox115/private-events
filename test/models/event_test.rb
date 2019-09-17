c795d3b7a08935af8551f30efefc5bf671a831ea
Carlos Del Real <carloshdelreal@gmail.com>
2019-09-17 07:11:17 -0500

    Events Testing Valid creation and validation

---
+++ test/models/event_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class EventTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  def setup
+    @user = users(:carlos)
+    @event = events(:event1)
+  end
+
+  test 'should be valid' do
+    assert @event.valid?
+  end
+  test 'title should be present' do
+    @event.title = '     '
+    assert_not @event.valid?
+  end
+  test 'description should be present' do
+    @event.description = '     '
+    assert_not @event.valid?
+  end
+end
