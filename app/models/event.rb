f653bd087666da3fe43f232598f0ca47b167664e
Carlos Del Real <carloshdelreal@gmail.com>
2019-09-19 17:19:16 -0500

    Add validation to the date field, as well as testing

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,7 +5,8 @@ class Event < ApplicationRecord
   has_many :guests, through: :attendances, source: "attendee"
   validates :date, presence: true
   validate :event_is_in_future, on: :create
-  validate :past_events_cant_change, on: :update 
+  validate :past_events_cant_change, on: :update
+  validate :cant_change_to_past, on: :update
   validates :title, presence: true
   validates :description, presence: true
 
@@ -17,10 +18,16 @@ class Event < ApplicationRecord
       errors.add(:date, "can't be in the past")
     end
   end
+  def cant_change_to_past
+    event = Event.find_by(id: id)
+    if date.present? && event.date != date && date < Date.today
+      errors.add(:date, "can't be in the past")
+    end
+  end
   def past_events_cant_change
     event = Event.find_by(id: id)
-    if date.present? && date < Date.today && event.date != date
-      errors.add(:date, "past events can't change")
+    if date.present? && event.date < Date.today
+      errors.add(:date, "past events can't be changed")
     end
   end
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -7,6 +7,7 @@ class EventTest < ActiveSupport::TestCase
   def setup
     @user = users(:carlos)
     @event = events(:event1)
+    @past_event = events(:past_event)
   end
 
   test 'should be valid' do
@@ -25,9 +26,24 @@ class EventTest < ActiveSupport::TestCase
     assert_not @event.valid?
   end
 
-  test 'event should be in the future' do
+  test 'Events in the past should not be valid' do
+    e = @user.events.new(title:"Future", description: "event", date: Time.now - 1.week)
+    assert_not e.valid?
+  end
+
+  test 'edit an event should be in the future' do
     @event.date = Time.now - 1.week
     assert_not @event.valid?
+  end
+
+  test 'event that is in the future could be re scheduled' do
+    @event.date = Time.now + 2.day
+    assert @event.valid?
+  end
+
+  test 'past events can not be rescheduled' do
+    @past_event.date = Time.now + 1.month
+    assert_not @past_event.valid?
   end
 
 end
